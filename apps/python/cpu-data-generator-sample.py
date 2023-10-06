ec140a6ff88a0797aaaedcb69a8e49a7ba6da99e
Gary Lu <67504821+gLuColte@users.noreply.github.com>
2023-10-07 00:25:40 +1100

    Influx DB initial

--- apps/python/cpu-data-generator-sample.py
+++ apps/python/cpu-data-generator-sample.py
@@ -21,9 +21,11 @@ INFLUX_DB_USERNAME = "admin"
 INFLUX_DB_PASSWORD = "admin123"
 INFLUX_DB_ORG = "ai-playground"
 
+
 # Generator Commands
 measurement_name = "census"
 bucket_name = "alien-observatory"
+datetime_format = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 ##########################################################
 ####################### Functions ########################
@@ -37,27 +39,16 @@ if __name__ == "__main__":
     # Initialize Influx DB
     client = influxdb_client.InfluxDBClient(
         url=INFLUX_DB_URL,
-        username=INFLUX_DB_USERNAME, 
-        password=INFLUX_DB_PASSWORD,
+        username='admin', 
+        password='admin123',
         org=INFLUX_DB_ORG
     )
     write_api = client.write_api(write_options=SYNCHRONOUS)
     # p = influxdb_client.Point(measurement_name).tag("location", "Prague").tag("id", uuid.uuid4()).field("temperature", 25.3)
     count=1
     while True:
-        for device_location_num in range(1,10):
-            p = influxdb_client.Point(
-                    measurement_name
-                ).tag(
-                    "location", f"Sector-{device_location_num:02d}"
-                ).field(
-                    "temperature", round(random.uniform(0, 40), 2)
-                ).field(
-                    "human_count", random.randint(0, 20)
-                ).field(
-                    "ailen_count", random.randint(0, 5)
-                )
-            write_api.write(bucket=bucket_name, org=INFLUX_DB_ORG, record=p)
+        p = influxdb_client.Point(measurement_name).tag("location", "Meblourne").field("temperature", round(random.uniform(0, 40), 2))
+        write_api.write(bucket=bucket_name, org=INFLUX_DB_ORG, record=p)
         print(f"Insert Count = {count}")
         time.sleep(1)
         count+=1
